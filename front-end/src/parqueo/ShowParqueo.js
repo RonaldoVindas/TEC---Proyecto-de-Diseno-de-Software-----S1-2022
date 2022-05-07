86a620606e3d8339da9b906871520fb91ae9ff96
Renzo Barra <rbarra@estudiantec.cr>
2022-05-06 23:44:33 -0600

    Parqueo

---
+++ front-end/src/parqueo/ShowParqueo.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+
+const URI = 'http://localhost:8000/agregarParqueo/'
+
+const ShowParqueoComponent = () => {
+  return (
+    <div>ShowParqueo</div>
+  )
+}
+
+export default ShowParqueoComponent
